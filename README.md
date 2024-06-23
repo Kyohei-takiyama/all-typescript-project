35f08235d6c9075edbf3bc054fc63960a333a55a
Kyohei-takiyama <takiyamakyohei@gmail.com>
2024-06-23 18:21:11 +0900

    add sample and readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # all-typescript-project
+
+## Overview
+
+Frontend , Backend , できれば Infra も全て Typescript で開発して、モノリスアーキテクチャを体験してみる
+
+## Architecture
+
+WIP
+
+## CI/CD
+
+WIP
+
+## How to Linking Issue
+
+- Refference:https://docs.github.com/en/issues/tracking-your-work-with-issues/linking-a-pull-request-to-an-issue
+
+```sh
+# commit
+ git commit -m "someting commit message {issue No}"
+
+ # PR
+ Someting PR title {Closes #{issue No}}
+```
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import App from "./App.tsx";
 import "./index.css";
+import { Login } from "./components/pages/Login";
+
+// ルーターの作成
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
