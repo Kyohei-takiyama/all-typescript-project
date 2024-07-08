620ac7790dc5a11bb0f7b0669744c3f742071ca5
Kyohei-takiyama <takiyamakyohei@gmail.com>
2024-07-08 22:41:47 +0900

    feat: code deploy #14

--- terraform/modules/cloudfront/main.tf
+++ terraform/modules/cloudfront/main.tf
@@ -42,11 +42,10 @@ resource "aws_cloudfront_distribution" "distribution" {
     acm_certificate_arn            = aws_acm_certificate.cert.arn
     ssl_support_method             = "sni-only"
     minimum_protocol_version       = "TLSv1.2_2021"
-    cloudfront_default_certificate = true
+    cloudfront_default_certificate = false
   }
 
 }
 
 resource "aws_cloudfront_origin_access_identity" "distribution" {
-  comment = "Access from CloudFront"
 }
